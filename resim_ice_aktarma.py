5df45098b362dd9f16c0125023fb712fafb82ba2
essmakoc <46110036+essmakoc@users.noreply.github.com>
2021-08-02 21:43:05 +0300

    Update resim_ice_aktarma.py

--- resim_ice_aktarma.py
+++ resim_ice_aktarma.py
@@ -1,6 +1,14 @@
 import cv2
 
-img = cv2.imread("Ahmet.jpg")
+img = cv2.imread("brad_pitt.jpg",0) # Sıfır resmi grayscale yapar 
 cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+
+k = cv2.waitKey(0) &0xFF
+
+if k == 27:   #wsc
+    cv2.destroyAllWindows()
+    
+elif k == ord('s'):
+    cv2.imwrite("brad_pitt_gray.jpg", img)
+    cv2.destroyAllWindows()
